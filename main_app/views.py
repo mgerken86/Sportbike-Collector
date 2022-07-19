fcb01c8c18494cef76680566856ea9c735dc8508
Mark Gerken <mgerken86@gmail.com>
2022-07-19 11:14:58 -0600

    model for sportbikes

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 class Sportbike:
-    def __init__(self, make, model, displacement, )
+    def __init__(self, make, model, displacement, skill_lvl):
+        self.make =  make
+        self.model = model
+        self.displacement = displacement
+        self.skill_lvl = skill_lvl
+
+sportbikes = [
+    Sportbike('Kawasaki', 'ZX-10R', "1000cc", 'Advanced'),
+    Sportbike('Yamaha', 'YZF-R1', "1000cc", 'Advanced'),
+    Sportbike('Honda', 'CBR-1000RR', "1000cc", 'Advanced'),
+    Sportbike('Triumph', 'Daytona 675', "675cc", 'Intermediate'),
+]
 
 # Define the home view
 def home(request):
